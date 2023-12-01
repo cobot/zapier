161ceca47d2503613e4bc3091d227001fa2a5318
Alexander Lang <alex@cobot.me>
2023-12-01 09:31:33 +0100

    fix labels

--- src/triggers/triggerExternalBooking.ts
+++ src/triggers/triggerExternalBooking.ts
@@ -1,34 +1,44 @@
-import { ZObject } from 'zapier-platform-core';
-import { KontentBundle } from '../types/kontentBundle';
-import { getExternalBookings, listRecentBookings, listRecentExternalBookings, subscribeHook, unsubscribeHook } from '../utils/api';
-import { get } from 'lodash';
-import { SubscribeBundleInputType } from '../types/subscribeType';
-import { getSubdomainField } from '../fields/getSudomainsField';
+import { ZObject } from "zapier-platform-core";
+import { KontentBundle } from "../types/kontentBundle";
+import {
+  getExternalBookings,
+  listRecentBookings,
+  listRecentExternalBookings,
+  subscribeHook,
+  unsubscribeHook,
+} from "../utils/api";
+import { get } from "lodash";
+import { SubscribeBundleInputType } from "../types/subscribeType";
+import { getSubdomainField } from "../fields/getSudomainsField";
 
+const hookLabel = "External Booking Created";
+const event = "created_booking";
 
-const hookLabel = 'External Booking created';
-const event = 'created_booking'
-
-async function subscribeHookExecute(z: ZObject, bundle: KontentBundle<SubscribeBundleInputType>) {
-  return subscribeHook(z, bundle, { 
+async function subscribeHookExecute(
+  z: ZObject,
+  bundle: KontentBundle<SubscribeBundleInputType>
+) {
+  return subscribeHook(z, bundle, {
     event,
-    callback_url: bundle.targetUrl ?? ''
-  })
+    callback_url: bundle.targetUrl ?? "",
+  });
 }
 
-async function unsubscribeHookExecute(z: ZObject, bundle: KontentBundle<SubscribeBundleInputType>) {
+async function unsubscribeHookExecute(
+  z: ZObject,
+  bundle: KontentBundle<SubscribeBundleInputType>
+) {
   const webhook = bundle.subscribeData;
-  return unsubscribeHook(z, bundle, webhook?.id ?? '')
+  return unsubscribeHook(z, bundle, webhook?.id ?? "");
 }
-
 
 async function parsePayload(z: ZObject, bundle: KontentBundle<{}>) {
-  const bookingId = get(bundle.cleanedRequest, 'id');
-  const response = await getExternalBookings(z, bookingId)
+  const bookingId = get(bundle.cleanedRequest, "id");
+  const response = await getExternalBookings(z, bookingId);
   if (response) {
     return [response];
   } else {
-    return []
+    return [];
   }
 }
 
@@ -38,46 +48,46 @@ export default {
   display: {
     important: true,
     label: hookLabel,
-    description: 'Triggers when an external booking is created.',
+    description: "Triggers when an external booking is created.",
   },
   operation: {
-    type: 'hook',
+    type: "hook",
 
-    inputFields: [
-      getSubdomainField(),
-    ],
+    inputFields: [getSubdomainField()],
 
     performSubscribe: subscribeHookExecute,
     performUnsubscribe: unsubscribeHookExecute,
 
     perform: parsePayload,
-    performList: (z: ZObject, bundle: KontentBundle<SubscribeBundleInputType>) =>
-      listRecentExternalBookings(z, bundle),
+    performList: (
+      z: ZObject,
+      bundle: KontentBundle<SubscribeBundleInputType>
+    ) => listRecentExternalBookings(z, bundle),
 
     sample: {
-      "id": "d58b612aaa62619aae546dd336587eb2",
-      "from": "2012-04-12T12:00:00.000Z",
-      "to": "2012-04-12T12:00:00.000Z",
-      "tax_rate": "20.0",
-      "title": "test booking",
-      "resource": {
-        "name": "Meeting Room",
-        "id": "12345"
+      id: "d58b612aaa62619aae546dd336587eb2",
+      from: "2012-04-12T12:00:00.000Z",
+      to: "2012-04-12T12:00:00.000Z",
+      tax_rate: "20.0",
+      title: "test booking",
+      resource: {
+        name: "Meeting Room",
+        id: "12345",
       },
-      "price": 10.0,
-      "has_custom_price": false,
-      "currency": "EUR",
-      "accounting_code": "B1",
-      "membership": {
-        "id": "123498y452346",
-        "name": "John Doe"
+      price: 10.0,
+      has_custom_price: false,
+      currency: "EUR",
+      accounting_code: "B1",
+      membership: {
+        id: "123498y452346",
+        name: "John Doe",
       },
-      "url": "https://co-up.cobot.me/api/bookings/20723075",
-      "can_change": true,
-      "comments": "coffee please",
-      "units": 1,
-      "created_at": "2012-04-12T12:00:00.000Z",
-      "updated_at": "2012-04-12T12:00:00.000Z"
+      url: "https://co-up.cobot.me/api/bookings/20723075",
+      can_change: true,
+      comments: "coffee please",
+      units: 1,
+      created_at: "2012-04-12T12:00:00.000Z",
+      updated_at: "2012-04-12T12:00:00.000Z",
     },
   },
 } as const;
--- src/triggers/triggerMembershipPlanChanged.ts
+++ src/triggers/triggerMembershipPlanChanged.ts
@@ -1,34 +1,43 @@
-import { ZObject } from 'zapier-platform-core';
-import { KontentBundle } from '../types/kontentBundle';
-import { apiCallUrl, listRecentBookings, subscribeHook, unsubscribeHook } from '../utils/api';
-import { SubscribeBundleInputType } from '../types/subscribeType';
-import { getSubdomainField } from '../fields/getSudomainsField';
+import { ZObject } from "zapier-platform-core";
+import { KontentBundle } from "../types/kontentBundle";
+import {
+  apiCallUrl,
+  listRecentBookings,
+  subscribeHook,
+  unsubscribeHook,
+} from "../utils/api";
+import { SubscribeBundleInputType } from "../types/subscribeType";
+import { getSubdomainField } from "../fields/getSudomainsField";
 
+const hookLabel = "Membership Plan Change Date Reached";
+const event = "membership_plan_change_date_reached";
 
-const hookLabel = 'Membership plan change date reached';
-const event = 'membership_plan_change_date_reached'
-
-async function subscribeHookExecute(z: ZObject, bundle: KontentBundle<SubscribeBundleInputType>) {
-  return subscribeHook(z, bundle, { 
+async function subscribeHookExecute(
+  z: ZObject,
+  bundle: KontentBundle<SubscribeBundleInputType>
+) {
+  return subscribeHook(z, bundle, {
     event,
-    callback_url: bundle.targetUrl ?? ''
-  })
+    callback_url: bundle.targetUrl ?? "",
+  });
 }
 
-async function unsubscribeHookExecute(z: ZObject, bundle: KontentBundle<SubscribeBundleInputType>) {
+async function unsubscribeHookExecute(
+  z: ZObject,
+  bundle: KontentBundle<SubscribeBundleInputType>
+) {
   const webhook = bundle.subscribeData;
-  return unsubscribeHook(z, bundle, webhook?.id ?? '')
+  return unsubscribeHook(z, bundle, webhook?.id ?? "");
 }
-
 
 async function parsePayload(z: ZObject, bundle: KontentBundle<{}>) {
   var membership;
   if (bundle.cleanedRequest) {
-    membership = await apiCallUrl(z, bundle.cleanedRequest.url)
-    membership.from = new Date(membership.from).toISOString()
-    membership.to = new Date(membership.to).toISOString()
-    membership.created_at = new Date(membership.created_at).toISOString()
-    membership.updated_at = new Date(membership.updated_at).toISOString()
+    membership = await apiCallUrl(z, bundle.cleanedRequest.url);
+    membership.from = new Date(membership.from).toISOString();
+    membership.to = new Date(membership.to).toISOString();
+    membership.created_at = new Date(membership.created_at).toISOString();
+    membership.updated_at = new Date(membership.updated_at).toISOString();
   }
   return [membership];
 }
@@ -42,21 +51,21 @@ export default {
     description: "Triggers on the day a member's new plan becomes active.",
   },
   operation: {
-    type: 'hook',
+    type: "hook",
 
-    inputFields: [
-      getSubdomainField(),
-    ],
+    inputFields: [getSubdomainField()],
 
     performSubscribe: subscribeHookExecute,
     performUnsubscribe: unsubscribeHookExecute,
 
     perform: parsePayload,
-    performList: (z: ZObject, bundle: KontentBundle<SubscribeBundleInputType>) =>
-      listRecentBookings(z, bundle),
+    performList: (
+      z: ZObject,
+      bundle: KontentBundle<SubscribeBundleInputType>
+    ) => listRecentBookings(z, bundle),
 
     sample: {
-      "url": "https://co-up.cobot.me/api/memberships/93207605"
+      url: "https://co-up.cobot.me/api/memberships/93207605",
     },
   },
 } as const;
