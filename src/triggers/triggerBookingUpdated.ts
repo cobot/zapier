190b43f66d835ce2fc3bffdc163186a0608678cd
Shpetim Selaci <shselaci3@gmail.com>
2026-02-25 16:19:10 +0100

    feat: more triggers

---
+++ src/triggers/triggerBookingUpdated.ts
@@ -0,0 +1,96 @@
+import { ZObject } from "zapier-platform-core";
+import { KontentBundle } from "../types/kontentBundle";
+import {
+  apiCallUrl,
+  getMembership,
+  listRecentBookings,
+  subscribeHook,
+  unsubscribeHook,
+} from "../utils/api";
+import { getSubdomainField } from "../fields/getSudomainsField";
+import { SubscribeBundleInputType } from "../types/subscribeType";
+import { bookingSample } from "../utils/samples";
+import { BookingOutput } from "../types/outputs";
+import { apiResponseToBookingOutput } from "../utils/api-to-output";
+import { BookingApiResponse } from "../types/api-responses";
+import { HookTrigger } from "../types/trigger";
+
+const hookLabel = "Booking Updated";
+const event = "updated_booking";
+
+async function subscribeHookExecute(
+  z: ZObject,
+  bundle: KontentBundle<SubscribeBundleInputType>,
+) {
+  return subscribeHook(z, bundle, {
+    event,
+    callback_url: bundle.targetUrl ?? "",
+  });
+}
+
+async function unsubscribeHookExecute(
+  z: ZObject,
+  bundle: KontentBundle<SubscribeBundleInputType>,
+) {
+  const webhook = bundle.subscribeData;
+  return unsubscribeHook(z, bundle, webhook?.id ?? "");
+}
+
+async function parsePayload(
+  z: ZObject,
+  bundle: KontentBundle<{}>,
+): Promise<BookingOutput[]> {
+  if (bundle.cleanedRequest) {
+    const booking = (await apiCallUrl(
+      z,
+      bundle.cleanedRequest.url,
+    )) as BookingApiResponse;
+    const subdomain = (bundle.inputData as any).subdomain as string;
+    const membershipId = booking.membership?.id;
+    if (membershipId && subdomain) {
+      const membership = await getMembership(z, subdomain, membershipId);
+      return [apiResponseToBookingOutput(booking, membership)];
+    }
+    return [apiResponseToBookingOutput(booking, null)];
+  } else {
+    return [];
+  }
+}
+
+const trigger: HookTrigger = {
+  key: event,
+  noun: hookLabel,
+  display: {
+    label: hookLabel,
+    description: "Triggers when a booking is updated.",
+  },
+  operation: {
+    type: "hook",
+
+    inputFields: [getSubdomainField()],
+
+    performSubscribe: subscribeHookExecute,
+    performUnsubscribe: unsubscribeHookExecute,
+
+    perform: parsePayload,
+    performList: async (
+      z: ZObject,
+      bundle: KontentBundle<SubscribeBundleInputType>,
+    ): Promise<BookingOutput[]> => {
+      const apiBookings = await listRecentBookings(z, bundle);
+      const bookingOutputPromises = apiBookings.map(async (b) => {
+        const subdomain = (bundle.inputData as any).subdomain as string;
+        const membershipId = b.membership?.id;
+        if (membershipId && subdomain) {
+          const membership = await getMembership(z, subdomain, membershipId);
+          return apiResponseToBookingOutput(b, membership);
+        }
+        return apiResponseToBookingOutput(b, null);
+      });
+      const bookingOutputs = await Promise.all(bookingOutputPromises);
+      return bookingOutputs;
+    },
+    sample: bookingSample,
+  },
+};
+export default trigger;
---
+++ src/triggers/triggerMembershipUncanceled.ts
@@ -0,0 +1,78 @@
+import { ZObject } from "zapier-platform-core";
+import { KontentBundle } from "../types/kontentBundle";
+import {
+  apiCallUrl,
+  listMemberships,
+  subscribeHook,
+  unsubscribeHook,
+} from "../utils/api";
+import { SubscribeBundleInputType } from "../types/subscribeType";
+import { getSubdomainField } from "../fields/getSudomainsField";
+import { MembershipOutput } from "../types/outputs";
+import { apiResponseToMembershipOutput } from "../utils/api-to-output";
+import { membershipSample } from "../utils/samples";
+import { HookTrigger } from "../types/trigger";
+
+const hookLabel = "Membership Uncanceled";
+const event = "uncanceled_membership";
+
+async function subscribeHookExecute(
+  z: ZObject,
+  bundle: KontentBundle<SubscribeBundleInputType>,
+) {
+  return subscribeHook(z, bundle, {
+    event,
+    callback_url: bundle.targetUrl ?? "",
+  });
+}
+
+async function unsubscribeHookExecute(
+  z: ZObject,
+  bundle: KontentBundle<SubscribeBundleInputType>,
+) {
+  const webhook = bundle.subscribeData;
+  return unsubscribeHook(z, bundle, webhook?.id ?? "");
+}
+
+async function parsePayload(
+  z: ZObject,
+  bundle: KontentBundle<{}>,
+): Promise<MembershipOutput[]> {
+  if (bundle.cleanedRequest) {
+    const membership = await apiCallUrl(z, bundle.cleanedRequest.url);
+    return [await apiResponseToMembershipOutput(membership, z)];
+  } else {
+    return [];
+  }
+}
+
+const trigger: HookTrigger = {
+  key: event,
+  noun: hookLabel,
+  display: {
+    label: hookLabel,
+    description: "Triggers when a membership cancellation is undone.",
+  },
+  operation: {
+    type: "hook",
+
+    inputFields: [getSubdomainField()],
+
+    performSubscribe: subscribeHookExecute,
+    performUnsubscribe: unsubscribeHookExecute,
+
+    perform: parsePayload,
+    performList: async (
+      z: ZObject,
+      bundle: KontentBundle<SubscribeBundleInputType>,
+    ): Promise<MembershipOutput[]> => {
+      const apiMemberships = await listMemberships(z, bundle);
+      return Promise.all(
+        apiMemberships.map((m) => apiResponseToMembershipOutput(m, z)),
+      );
+    },
+
+    sample: membershipSample,
+  },
+};
+export default trigger;
